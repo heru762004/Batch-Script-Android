3553df2c4b7fa148d4d6fe004b9c42c6c7560cfe
Heru Prasetia <heru762004@yahoo.com>
2017-03-31 15:34:04 +0800

    initial uninstall apk

---
+++ uninstall.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+echo APK Uninstaller v1.0
+echo created by Heru Prasetia
+ECHO "%~1"
+set AAPT=<<path to aapt>>
+FOR %%i IN ("%~1") DO (
+SET filedrive=%%~di
+SET filepath=%%~pi
+SET filename=%%~ni
+SET fileextension=%%~xi
+)
+SET fPath=%filedrive%%filepath%
+ECHO "%fPath%"
+cd "%fPath%"
+SET P=%filename%%fileextension%
+echo %P%
+set X=%AAPT% d badging %P%
+for /f "delims=' tokens=2,4" %%a in ('%X%') do (
+    echo Package name is %%a
+    echo Version code is %%b
+	set Y=%%a
+	goto :break
+)
+:break
+echo %Y%
+adb shell am start -a android.intent.action.DELETE -d package:%Y%
+pause
